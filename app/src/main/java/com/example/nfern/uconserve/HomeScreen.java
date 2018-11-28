2d5e6e5a44a1c5bb2985ceac763534d48d82ff23
Ferneza <36428382+Ferneza@users.noreply.github.com>
2018-11-28 15:55:12 -0500

    Draft for home screen button functionality

--- app/src/main/java/com/example/nfern/uconserve/HomeScreen.java
+++ app/src/main/java/com/example/nfern/uconserve/HomeScreen.java
@@ -2,6 +2,7 @@ package com.example.nfern.uconserve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        Button feedbackBtn = (Button)findViewById(R.id.feedbackBtn);
+        feedbackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent startIntent = new Intent(getApplicationContext(), Feedback.class);
+                startActivity(startIntent);
+
+            }
+        });
     }
 }
